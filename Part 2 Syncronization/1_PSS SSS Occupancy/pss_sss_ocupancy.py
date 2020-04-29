24d969fee1dbec5df8de1594601bdb8f99ec7490
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-04-29 16:34:38 +0900

    v1.3_Part2_PSS&SSS
    Update Codes

--- Part 2 Syncronization/1_PSS SSS Occupancy/pss_sss_ocupancy.py
+++ Part 2 Syncronization/1_PSS SSS Occupancy/pss_sss_ocupancy.py
@@ -28,7 +28,7 @@ df_fr2['Normal CP RE/Sec'] = df_fr2['Normal CP RE/Frame']*100
 # FR1
 
 # For FR1, SSB Count in SSB Burst can be up until 4 and 8 (LMax = 4 for < 3GHz, LMax = 8 for ~3-6GHz)
-# For SSB Periodicity, it can be set to 5, 10, 15, 20, 40, 80, 160 ms based on spec
+# For SSB Periodicity, it can be set to 5, 10, 20, 40, 80, 160 ms based on spec
 # PSS Size: 127 Subcarriers, 1 Symbol
 # SSS Size: 127 Subcarriers, 1 Symbol
 # PSS + SSS Occupancy/Sec = ((PSS Size+SSS Size)*L*SSB Burst Count in 1 Sec)/Total RE per Sec
@@ -38,7 +38,7 @@ sss_size = 127*1
 sec = 1000
 
 
-p = [5, 10, 15, 20, 40, 80, 160]  # SSB Burst Periodicity
+p = [5, 10, 20, 40, 80, 160]  # SSB Burst Periodicity
 i = 0                             # Counter
 index = 0
 
@@ -73,11 +73,10 @@ df_fr1_scs15 = df_fr1_scs15.drop(['FR', 'SCS (kHz)', 'RB', 'ID', 'Slots/Subframe
 df_fr1_scs15.set_index('Bandwidth (MHz)', inplace=True)
 df_fr1_scs15_5ms = df_fr1_scs15.iloc[:,:4]
 df_fr1_scs15_10ms = df_fr1_scs15.iloc[:,4:8]
-df_fr1_scs15_15ms = df_fr1_scs15.iloc[:,8:12]
-df_fr1_scs15_20ms = df_fr1_scs15.iloc[:,12:16]
-df_fr1_scs15_40ms = df_fr1_scs15.iloc[:,16:20]
-df_fr1_scs15_80ms = df_fr1_scs15.iloc[:,20:24]
-df_fr1_scs15_160ms = df_fr1_scs15.iloc[:,24:28]
+df_fr1_scs15_20ms = df_fr1_scs15.iloc[:,8:12]
+df_fr1_scs15_40ms = df_fr1_scs15.iloc[:,12:16]
+df_fr1_scs15_80ms = df_fr1_scs15.iloc[:,16:20]
+df_fr1_scs15_160ms = df_fr1_scs15.iloc[:,20:24]
 
 df_fr1_scs30 = df_fr1[df_fr1['SCS (kHz)'] == 30]
 df_fr1_scs30 = df_fr1_scs30.drop(['FR', 'SCS (kHz)', 'RB', 'ID', 'Slots/Subframe', 'Slots/Frame',
@@ -85,16 +84,15 @@ df_fr1_scs30 = df_fr1_scs30.drop(['FR', 'SCS (kHz)', 'RB', 'ID', 'Slots/Subframe
 df_fr1_scs30.set_index('Bandwidth (MHz)', inplace=True)
 df_fr1_scs30_5ms = df_fr1_scs30.iloc[:,:4]
 df_fr1_scs30_10ms = df_fr1_scs30.iloc[:,4:8]
-df_fr1_scs30_15ms = df_fr1_scs30.iloc[:,8:12]
-df_fr1_scs30_20ms = df_fr1_scs30.iloc[:,12:16]
-df_fr1_scs30_40ms = df_fr1_scs30.iloc[:,16:20]
-df_fr1_scs30_80ms = df_fr1_scs30.iloc[:,20:24]
-df_fr1_scs30_160ms = df_fr1_scs30.iloc[:,24:28]
+df_fr1_scs30_20ms = df_fr1_scs30.iloc[:,8:12]
+df_fr1_scs30_40ms = df_fr1_scs30.iloc[:,12:16]
+df_fr1_scs30_80ms = df_fr1_scs30.iloc[:,16:20]
+df_fr1_scs30_160ms = df_fr1_scs30.iloc[:,20:24]
 
 # FR2
 
 # For FR2, SSB Count in SSB Burst can be up until 64 (LMax = 64)
-# For SSB Periodicity, it can be set to 5, 10, 15, 20, 40, 80, 160 ms based on spec
+# For SSB Periodicity, it can be set to 5, 10, 20, 40, 80, 160 ms based on spec
 # PSS Size: 127 Subcarriers, 1 Symbol
 # SSS Size: 127 Subcarriers, 1 Symbol
 # PSS + SSS Occupancy/Sec = ((PSS Size+SSS Size)*L*SSB Burst Count in 1 Sec)/Total RE per Sec
@@ -104,7 +102,7 @@ sss_size = 127*1
 sec = 1000
 
 
-p = [5, 10, 15, 20, 40, 80, 160]  # SSB Burst Periodicity
+p = [5, 10, 20, 40, 80, 160]  # SSB Burst Periodicity
 i = 0                             # Counter
 index = 0
 
@@ -139,11 +137,10 @@ df_fr2_scs120 = df_fr2_scs120.drop(['FR', 'SCS (kHz)', 'RB', 'ID', 'Slots/Subfra
 df_fr2_scs120.set_index('Bandwidth (MHz)', inplace=True)
 df_fr2_scs120_5ms = df_fr2_scs120.iloc[:,:7]
 df_fr2_scs120_10ms = df_fr2_scs120.iloc[:,7:14]
-df_fr2_scs120_15ms = df_fr2_scs120.iloc[:,14:21]
-df_fr2_scs120_20ms = df_fr2_scs120.iloc[:,21:28]
-df_fr2_scs120_40ms = df_fr2_scs120.iloc[:,28:35]
-df_fr2_scs120_80ms = df_fr2_scs120.iloc[:,35:42]
-df_fr2_scs120_160ms = df_fr2_scs120.iloc[:,42:49]
+df_fr2_scs120_20ms = df_fr2_scs120.iloc[:,14:21]
+df_fr2_scs120_40ms = df_fr2_scs120.iloc[:,21:28]
+df_fr2_scs120_80ms = df_fr2_scs120.iloc[:,28:35]
+df_fr2_scs120_160ms = df_fr2_scs120.iloc[:,35:42]
 
 df_fr2_scs240 = df_fr2[df_fr2['SCS (kHz)'] == 240]
 df_fr2_scs240 = df_fr2_scs240.drop(['FR', 'SCS (kHz)', 'RB', 'ID', 'Slots/Subframe', 'Slots/Frame',
@@ -152,50 +149,47 @@ df_fr2_scs240.set_index('Bandwidth (MHz)', inplace=True)
 
 # Plot
 
-fig1, axes = plt.subplots(nrows=3, ncols=3)
+fig1, axes = plt.subplots(nrows=2, ncols=3)
 
 df_fr1_scs15_5ms.plot(kind='line', style='.--', ax=axes[0,0], ylim=[0,15], xlim=[0,100])
 df_fr1_scs15_10ms.plot(kind='line', style='.--', ax=axes[0,1], ylim=[0,15], xlim=[0,100])
-df_fr1_scs15_15ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,100])
-df_fr1_scs15_20ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,100])
-df_fr1_scs15_40ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,100])
-df_fr1_scs15_80ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,100])
-df_fr1_scs15_160ms.plot(kind='line', style='.--', ax=axes[2,0], ylim=[0,2], xlim=[0,100])
+df_fr1_scs15_20ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,100])
+df_fr1_scs15_40ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,100])
+df_fr1_scs15_80ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,100])
+df_fr1_scs15_160ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,100])
 
 fig1.suptitle('PSS+SSS Occupancy (%) for FR = 1, SCS = 15 kHz\nL = SSB Count in SSB Burst Set, p = SSB Periodicity')
-fig1.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
+# fig1.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
 fig1.text(0.04, 0.5, 'Occupancy (%)', va='center', rotation='vertical')
 
 plt.show()
 
-fig2, axes = plt.subplots(nrows=3, ncols=3)
+fig2, axes = plt.subplots(nrows=2, ncols=3)
 
 df_fr1_scs30_5ms.plot(kind='line', style='.--', ax=axes[0,0], ylim=[0,15], xlim=[0,100])
 df_fr1_scs30_10ms.plot(kind='line', style='.--', ax=axes[0,1], ylim=[0,15], xlim=[0,100])
-df_fr1_scs30_15ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,100])
-df_fr1_scs30_20ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,100])
-df_fr1_scs30_40ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,100])
-df_fr1_scs30_80ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,100])
-df_fr1_scs30_160ms.plot(kind='line', style='.--', ax=axes[2,0], ylim=[0,2], xlim=[0,100])
+df_fr1_scs30_20ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,100])
+df_fr1_scs30_40ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,100])
+df_fr1_scs30_80ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,100])
+df_fr1_scs30_160ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,100])
 
 fig2.suptitle('PSS+SSS Occupancy (%) for FR = 1, SCS = 30 kHz\nL = SSB Count in SSB Burst Set, p = SSB Periodicity')
-fig2.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
+# fig2.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
 fig2.text(0.04, 0.5, 'Occupancy (%)', va='center', rotation='vertical')
 
 plt.show()
 
-fig3, axes = plt.subplots(nrows=3, ncols=3)
+fig3, axes = plt.subplots(nrows=2, ncols=3)
 
 df_fr2_scs120_5ms.plot(kind='line', style='.--', ax=axes[0,0], ylim=[0,15], xlim=[0,400])
 df_fr2_scs120_10ms.plot(kind='line', style='.--', ax=axes[0,1], ylim=[0,15], xlim=[0,400])
-df_fr2_scs120_15ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,400])
-df_fr2_scs120_20ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,400])
-df_fr2_scs120_40ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,400])
-df_fr2_scs120_80ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,400])
-df_fr2_scs120_160ms.plot(kind='line', style='.--', ax=axes[2,0], ylim=[0,2], xlim=[0,400])
+df_fr2_scs120_20ms.plot(kind='line', style='.--', ax=axes[0,2], ylim=[0,15], xlim=[0,400])
+df_fr2_scs120_40ms.plot(kind='line', style='.--', ax=axes[1,0], ylim=[0,5], xlim=[0,400])
+df_fr2_scs120_80ms.plot(kind='line', style='.--', ax=axes[1,1], ylim=[0,5], xlim=[0,400])
+df_fr2_scs120_160ms.plot(kind='line', style='.--', ax=axes[1,2], ylim=[0,5], xlim=[0,400])
 
 fig3.suptitle('PSS+SSS Occupancy (%) for FR = 2, SCS = 120 kHz\nL = SSB Count in SSB Burst Set, p = SSB Periodicity')
-fig3.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
+# fig3.text(0.5, 0.04, 'Bandwidth (MHz)', ha='center')
 fig3.text(0.04, 0.5, 'Occupancy (%)', va='center', rotation='vertical')
 
 plt.show()
