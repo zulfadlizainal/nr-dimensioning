9699b5f130621d2086af01a0402e962d7f2942b7
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2021-10-22 18:36:43 +0900

    v1.6_Part1_MinGuardBand
    Update codes

--- Part 1 Operating Band, Frame Structure/2_Minimum Guard Band/min_guard_band.py
+++ Part 1 Operating Band, Frame Structure/2_Minimum Guard Band/min_guard_band.py
@@ -9,20 +9,72 @@ import matplotlib.pyplot as plt
 df_gb_fr1 = pd.read_excel('3GPP_TS_38_101_GB_FR1.xlsx', index_col=0)
 df_gb_fr2 = pd.read_excel('3GPP_TS_38_101_GB_FR2.xlsx', index_col=0)
 
-# Plot FR1
-df_gb_fr1.plot.bar()
-plt.title('Minimum Guard Band for every BW & SCS - FR1 (kHz)')
-plt.ylabel('Minimum Guard Band (kHz)')
-plt.xlabel('Sub Carrier Spacing (SCS) (kHz)')
-plt.ylim(0, 10000)
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='NR Bandwidth')
+# Process FR1
+df_gb_fr1 = df_gb_fr1.transpose()
+df_gb_fr1['BW'] = df_gb_fr1.index
+df_gb_fr1['BW'] = df_gb_fr1['BW'].map(lambda x: x.lstrip('').rstrip(' MHz'))
+df_gb_fr1['BW']=df_gb_fr1['BW'].astype('int64')
+df_gb_fr1.set_index('BW', inplace=True)
 
-# Plot FR2
-df_gb_fr2.plot.bar()
-plt.title('Minimum Guard Band for every BW & SCS - FR2 (kHz)')
-plt.ylabel('Minimum Guard Band (kHz)')
-plt.xlabel('Sub Carrier Spacing (SCS) (kHz)')
+df_gb_fr1_ratio = df_gb_fr1/1000
+df_gb_fr1_ratio = df_gb_fr1_ratio.divide(df_gb_fr1_ratio.index, axis=0 )
+df_gb_fr1_ratio = df_gb_fr1_ratio*100
+
+# Process FR2
+df_gb_fr2 = df_gb_fr2.transpose()
+df_gb_fr2['BW'] = df_gb_fr2.index
+df_gb_fr2['BW'] = df_gb_fr2['BW'].map(lambda x: x.lstrip('').rstrip(' MHz'))
+df_gb_fr2['BW']=df_gb_fr2['BW'].astype('int64')
+df_gb_fr2.set_index('BW', inplace=True)
+
+df_gb_fr2_ratio = df_gb_fr2/1000
+df_gb_fr2_ratio = df_gb_fr2_ratio.divide(df_gb_fr2_ratio.index, axis=0 )
+df_gb_fr2_ratio = df_gb_fr2_ratio*100
+
+# Plot FR1 - Guardband kHz
+df_gb_fr1.plot(style=['v-','s-','8-'], markersize=10)
+plt.title('Guardband for every BW & SCS - FR1', fontsize=15)
+plt.ylabel('Guardband (kHz)')
+plt.xlabel('NR Bandwidth (MHz)')
+plt.ylim(0, 5000)
+plt.xlim(0, 100)
+plt.grid()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='(SCS) (kHz)')
+
+plt.show()
+
+# Plot FR1 - Guardband %
+df_gb_fr1_ratio.plot(style=['v-','s-','8-'], markersize=10)
+plt.title('Guardband Occupation (%) - FR1', fontsize=15)
+plt.ylabel('Guardband %')
+plt.xlabel('NR Bandwidth (MHz)')
+plt.ylim(0, 20)
+plt.xlim(0, 100)
+plt.grid()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='(SCS) (kHz)')
+
+plt.show()
+
+# Plot FR2 - Guardband kHz
+df_gb_fr2.plot(style=['v-','s-'], markersize=10)
+plt.title('Guardband for every BW & SCS - FR2', fontsize=15)
+plt.ylabel('Guardband (kHz)')
+plt.xlabel('NR Bandwidth (MHz)')
 plt.ylim(0, 10000)
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='NR Bandwidth')
+plt.xlim(0, 450)
+plt.grid()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='(SCS) (kHz)')
+
+plt.show()
+
+# Plot FR2 - Guardband %
+df_gb_fr2_ratio.plot(style=['v-','s-'], markersize=10)
+plt.title('Guardband Occupation (%) - FR2', fontsize=15)
+plt.ylabel('Guardband %')
+plt.xlabel('NR Bandwidth (MHz)')
+plt.ylim(0, 20)
+plt.xlim(0, 450)
+plt.grid()
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), title='(SCS) (kHz)')
 
 plt.show()
