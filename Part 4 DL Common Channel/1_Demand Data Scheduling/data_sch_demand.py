76662620657e6821b0307a0d9cc9b4cd19348796
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2021-10-26 18:55:32 +0900

    v1.1_Part4_DataDemand
    Update code

--- Part 4 DL Common Channel/1_Demand Data Scheduling/data_sch_demand.py
+++ Part 4 DL Common Channel/1_Demand Data Scheduling/data_sch_demand.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 dl_act_factor = 30/100         # Unit: %
 ul_act_factor = 10/100         # Unit: %
 
-mo_avgrrcuser_nbh = 5          # Unit: Avg RRC User / Hour
-mt_avgrrcuser_nbh = 2          # Unit: Avg RRC User / Hour
+mo_avgrrcuser_nbh = 5          # Unit: Avg RRC User / UE / Hour
+mt_avgrrcuser_nbh = 2          # Unit: Avg RRC User / UE / Hour
 
-sch_periodicity = 4            # Unit: ms
-sch_periodicity_frame = 10/4   # Unit: ms
+sch_periodicity = 4                          # Unit: ms
+sch_periodicity_frame = 10/sch_periodicity   # Unit: ms
 
-bler = 10/100                  # Unit %
+bler = 10/100                   # Unit %
 
 short_user_duration = 20/3600   # Unit: s/3600 (Erlang)
 mid_user_duration = 60/3600     # Unit: s/3600 (Erlang)
@@ -40,7 +40,7 @@ df_subs.plot(style=['v--', 's--', '8--'], markersize=5, markerfacecolor='None', 
              x='Subscribers', y=['Short User (20 Sec)', 'Mid User (60 Sec)', 'Long User (300 Sec)'])
 
 plt.title(
-    f'[NR DATA]\nNumber of Scheduling Needed / 10ms Frame\n\nAssumptions:\nUtilized Period / RRC Session (DL+UL) = {(dl_act_factor+ul_act_factor)*100} %\nNumber of RRC / UE / Hour (MO+MT) = {mo_avgrrcuser_nbh+mt_avgrrcuser_nbh}\nScheduling Periodicity = {sch_periodicity} ms\nBLER = {bler*100}%\n', fontsize=10)
+    f'[NR DATA]\nNumber of Scheduling Needed / 10ms Frame\n\nAssumptions:\nUtilized Period [RRC Session (DL+UL)] = {(dl_act_factor+ul_act_factor)*100} %\nNumber of RRC / UE / Hour (MO+MT) = {mo_avgrrcuser_nbh+mt_avgrrcuser_nbh}\nScheduling Periodicity = {sch_periodicity} ms\nBLER = {bler*100}%\n', fontsize=10)
 plt.ylabel('Scheduling Needed')
 plt.xlabel('Number of UE / Cell')
 plt.ylim(0, 180)
