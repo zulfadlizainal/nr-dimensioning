f9e359acd8e3dd0ee3a8c030ab11f232a6881b0c
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2021-12-29 14:03:11 +0900

    v2.5_Part6_Paging
    Update codes

--- Part 6 Paging/1_Paging Block Probability/__main__.py
+++ Part 6 Paging/1_Paging Block Probability/__main__.py
@@ -2,7 +2,7 @@
 # Github: https://github.com/zulfadlizainal
 # Linkedin: https://linkedin.com/in/zulfadlizainal
 
-from script import *
+from __script__ import *
 
 if __name__ == '__main__':
     pag_blk_dem = paging_block_demand()
